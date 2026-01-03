2f97975884a7e3707d7c8a56b4e9415e76cbbb22
Joshua Miller <j@ozmillers.com>
2026-01-03 16:54:45 +1030

    script now runs npm `clean` and `manual:build`

--- push-production.sh
+++ push-production.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/dash
 cp prod_data.json src/_data/data.json
+echo "Make sure you have stopped all instances of the npm dev server"
+echo "You can check this by running a new one (npm run dev) and see if"
+echo "port 8080 is bound already (localhost:8081 suggests otherwise)"
+sleep 6
+npm run clean && npm run build:manual
