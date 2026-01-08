34dbd3ef9bb43c6e1242e2d6d111eafa531deaf7
Joshua Miller <j@ozmillers.com>
2026-01-08 15:45:44 +1030

    Update /public

--- push-production.sh
+++ push-production.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/dash
+
 cp -fv prod_data.json src/_data/data.json
 echo "Make sure you have stopped all instances of the npm dev server"
 echo "You can check this by running a new one (npm run dev) and see if"
 echo "port 8080 is bound already (localhost:8081 suggests otherwise)"
-sleep 6
+if [ "$1" != "/now" ]; then
+    sleep 6
+fi
 npm run clean && npm run manual:build
 # git push origin main
