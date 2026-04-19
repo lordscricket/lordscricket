fe45f7bb8eef6fef117f3eb91580de5b87d8bcf3
Joshua Miller <260371239+thejoshuam@users.noreply.github.com>
2026-04-19 18:16:15 +0930

    tweak build script

--- push-production.sh
+++ push-production.sh
@@ -5,8 +5,8 @@ echo "Make sure you have stopped all instances of the npm dev server"
 echo "You can check this by running a new one (npm run dev) and see if"
 echo "port 8080 is bound already (localhost:8081 suggests otherwise)"
 if [ "$1" != "/now" ]; then
-    sleep 6
+    sleep 2
 fi
 npm run clean && npm run manual:build
-git commit -m src/_data/data/data.json
+git commit -mUpdateData src/_data/data.json && git push
 # git push origin main
